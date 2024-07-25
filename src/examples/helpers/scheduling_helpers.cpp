7fe965a3c34db4186cf71ce790acaab1c2005f99
Tim Luchterhand <tim.luchterhand@laas.fr>
2024-07-25 11:59:10 +0200

    added scheduling instance parsing helpers

---
+++ src/examples/helpers/scheduling_helpers.cpp
@@ -0,0 +1,41 @@
+/**
+* @author Tim Luchterhand
+* @date 25.07.24
+* @brief contains utility that allows to easily load and solve scheduling problems
+*/
+
+#include "scheduling_helpers.hpp"
+#include "util/SchedulingProblemHelper.hpp"
+#include "util/parsing/osp.hpp"
+
+
+auto loadSchedulingProblem(const tempo::Options &options) -> std::pair<SolverPtr, ProblemInstance> {
+    using namespace tempo;
+    auto solver = std::make_unique<Solver<>>(options);
+    auto schedule{solver->newInterval(0, Constant::Infinity<int>, 0, 0, 0, Constant::Infinity<int>)};
+    std::vector<DisjunctiveResource<>> resources;
+    std::vector<Interval<>> tasks;
+
+    if (options.input_format == "osp") {
+        osp::parse(options.instance_file, *solver, schedule, tasks, resources);
+    } else {
+        std::cerr << "problem type " << options.input_format << " is not (yet) supported" << std::endl;
+        std::exit(1);
+    }
+
+    SchedulingProblemHelper problem(std::move(tasks), std::move(resources), {}, schedule);
+
+    for (const auto &consumingTasks: problem.resources()) {
+        auto constraint = NoOverlap(schedule, consumingTasks);
+        solver->post(constraint);
+    }
+
+    auto trivialUb = 0;
+    for (const auto &t : problem.tasks()) {
+        trivialUb += t.minDuration(*solver);
+    }
+
+    trivialUb = std::min(trivialUb, options.ub);
+    solver->set(schedule.end.before(trivialUb));
+    return {std::move(solver), std::move(problem)};
+}
--- src/examples/torch/scheduler_gnn.cpp
+++ src/examples/torch/scheduler_gnn.cpp
@@ -4,27 +4,11 @@
 * @brief
 */
 
-#include <vector>
-
-#include "Solver.hpp"
 #include "heuristics/GNNValueHeuristics.hpp"
 #include "heuristics/heuristic_factories.hpp"
-#include "util/SchedulingProblemHelper.hpp"
 #include "util/parsing/jsp.hpp"
-#include "util/parsing/osp.hpp"
 #include "../helpers/cli.hpp"
-
-
-template<typename T = int>
-class DisjunctiveResource : public vector<tempo::Interval<T>> {
-public:
-    using vector<tempo::Interval<T>>::vector;
-
-    static constexpr auto resourceCapacity() noexcept { return 1; }
-
-    static constexpr auto getDemand(unsigned) noexcept { return 1; }
-};
-
+#include "../helpers/scheduling_helpers.hpp"
 
 
 int main(int argc, char **argv) {
@@ -36,36 +20,12 @@ int main(int argc, char **argv) {
                                  cli::ArgSpec("gnn-loc", "Location of the GNN model", true, gnnLocation),
                                  cli::ArgSpec("feat-config", "Location of the feature extractor config", true,
                                               featureExtractorConf));
-    Solver<> S(opt);
-    auto schedule{S.newInterval(0, Constant::Infinity<int>, 0, 0, 0, Constant::Infinity<int>)};
-    std::vector<DisjunctiveResource<>> resources;
-    std::vector<Interval<>> tasks;
 
-    if (opt.input_format == "osp") {
-        osp::parse(opt.instance_file, S, schedule, tasks, resources);
-    } else {
-        std::cerr << "problem type " << opt.input_format << " is not (yet) supported" << std::endl;
-        std::exit(1);
-    }
-
-    SchedulingProblemHelper problem(std::move(tasks), std::move(resources), {}, schedule);
-
-    for (const auto &consumingTasks: problem.resources()) {
-        auto constraint = NoOverlap(schedule, consumingTasks);
-        S.post(constraint);
-    }
-
-    auto trivialUb = 0;
-    for (const auto &t : problem.tasks()) {
-        trivialUb += t.minDuration(S);
-    }
-
-    trivialUb = std::min(trivialUb, opt.ub);
-    S.set(schedule.end.before(trivialUb));
-
+    auto [solver, problem] = loadSchedulingProblem(opt);
+    auto schedule = problem.schedule();
     GNNFullGuidance valBranching(opt.polarity_epsilon, gnnLocation, featureExtractorConf, std::move(problem));
-    auto varBranching = make_variable_heuristic(S);
-    S.setBranchingHeuristic(make_compound_heuristic(std::move(varBranching), std::move(valBranching)));
-    S.minimize(schedule.end);
+    auto varBranching = make_variable_heuristic(*solver);
+    solver->setBranchingHeuristic(make_compound_heuristic(std::move(varBranching), std::move(valBranching)));
+    solver->minimize(schedule.end);
     return 0;
 }
