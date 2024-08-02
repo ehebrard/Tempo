3dc85bf5a9e27cb45940d094ed8450f384faa62e
Tim Luchterhand <tim.luchterhand@laas.fr>
2024-08-02 11:26:01 +0200

    added info json to data-generation

--- src/examples/scheduling_data_generation.cpp
+++ src/examples/scheduling_data_generation.cpp
@@ -5,11 +5,14 @@
 */
 
 #include <string>
+#include <nlohmann/json.hpp>
 
 #include "helpers/cli.hpp"
 #include "helpers/scheduling_helpers.hpp"
+#include "helpers/shell.hpp"
 #include "data_generation.hpp"
 
+constexpr auto InfoFileName = "info.json";
 
 int main(int argc, char **argv) {
     using namespace tempo;
@@ -19,15 +22,27 @@ int main(int argc, char **argv) {
     std::string saveTo;
     auto options = cli::parseOptions(argc, argv,
                                      cli::ArgSpec("save-to", "Where to save the data points", true, saveTo));
-    auto [solver, problem, _] = loadSchedulingProblem(options);
+    auto [solver, problem, opt] = loadSchedulingProblem(options);
     auto schedule = problem.schedule();
     auto heuristic = make_compound_heuristic(make_variable_heuristic(*solver), TightestSolutionGuided(0, 0));
     solver->setBranchingHeuristic(std::move(heuristic));
     const auto problemName = fs::path(options.instance_file).filename();
     const auto destinationFolder = fs::path(saveTo) / problemName;
     DataGenerator dataGenerator(*solver, schedule, destinationFolder);
-    fs::copy(options.instance_file, destinationFolder, fs::copy_options::overwrite_existing);
     solver->minimize(schedule.duration);
+    std::optional<int> makespan;
+    if (solver->numeric.hasSolution()) {
+        makespan = solver->numeric.lower(schedule.duration);
+    }
+
+    fs::copy(options.instance_file, destinationFolder, fs::copy_options::overwrite_existing);
+    nlohmann::json meta;
+    meta["date"] = shell::getTimeStamp();
+    meta["commit"] = shell::getCommit();
+    meta["numSubProblems"] = dataGenerator.problemCount();
+    meta["numSolutions"] = dataGenerator.solutionCount();
+    meta["bestSolutionKnown"] = std::min(opt, makespan);
+    serialization::serializeToFile(meta, destinationFolder / InfoFileName);
     return 0;
 }
 
