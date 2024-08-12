74815fdad9f6a6bf8b4c6769330a9f69810f44df
Tim Luchterhand <tim.luchterhand@laas.fr>
2024-08-12 17:20:07 +0200

    added variable_importance program

---
+++ src/examples/helpers/VarImportanceRunner.cpp
@@ -0,0 +1,98 @@
+/**
+* @author Tim Luchterhand
+* @date 08.08.24
+* @brief
+*/
+
+#include <ranges>
+#include <Iterators.hpp>
+
+#include "VarImportanceRunner.hpp"
+#include "scheduling_helpers.hpp"
+
+VarImportanceRunner::VarImportanceRunner(ser::PartialProblem partialProblem, tempo::Options options,
+                                         const ser::Solution<Time> &optSol) : problem(std::move(partialProblem)),
+                                                                                 options(std::move(options)),
+                                                                                 optimum(optSol.objective) {
+    auto [s, p, _] = loadSchedulingProblem(this->options);
+    try {
+        loadBranch(*s, problem.decisions);
+    } catch (const tempo::Failure<Time> &) {
+        inconsistent = true;
+        return;
+    }
+
+    tempo::var_t maxVar = 0;
+    for (auto var : s->getBranch()) {
+        maxVar = std::max(maxVar, var);
+        if (s->boolean.hasSemantic(var)) {
+            auto edge = s->boolean.getEdge(true, var);
+            if (p.hasVariable(edge.from) and p.hasVariable(edge.to)) {
+                searchLiterals.emplace_back(s->boolean.getLiteral(true, var));
+            }
+        }
+    }
+
+    literalCache = decltype(literalCache)(maxVar * 2 + 2, false);
+    for (const auto &[var, val] : optSol.decisions) {
+        if (val and var <= maxVar) {
+            auto lit = s->boolean.getLiteral(val, var);
+            literalCache.at(lit) = true;
+        }
+    }
+}
+
+auto VarImportanceRunner::getLiterals() const noexcept -> const std::vector<tempo::Literal<Time>> & {
+    return searchLiterals;
+}
+
+double VarImportanceRunner::averageSearchEffort() const noexcept {
+    return averageNumberOfDecisions() / static_cast<double>(searchLiterals.size());
+}
+
+double VarImportanceRunner::averageNumberOfDecisions() const noexcept {
+    return static_cast<double>(totalNumDecisions) / numSearches;
+}
+
+auto VarImportanceRunner::run(tempo::Literal<Time> lit) -> Result {
+    using enum SchedulerState;
+    if (isInconsistent()) {
+        throw std::runtime_error("inconsistent sub problem");
+    }
+
+    auto [s, p, _] = loadSchedulingProblem(options);
+    loadBranch(*s, problem.decisions);
+    if (s->boolean.satisfied(lit) or s->boolean.falsified(lit)) {
+        return {AlreadyDecided, {}};
+    }
+
+    if (literalCache.at(lit)) {
+        return {Valid, optimum};
+    }
+
+    try {
+        s->set(lit);
+    } catch(const tempo::Failure<Time> &) {
+        return {Unsat, {}};
+    }
+
+    s->minimize(p.schedule().duration);
+    totalNumDecisions += s->num_choicepoints;
+    ++numSearches;
+    if (s->boolean.hasSolution() and s->numeric.hasSolution()) {
+        const auto makeSpan = s->numeric.lower(p.schedule().duration);
+        if (makeSpan == optimum) {
+            for (auto [litId, cacheVal] : iterators::enumerate(literalCache)) {
+                cacheVal = cacheVal or s->boolean.bestSolution().at(litId);
+            }
+        }
+
+        return {Valid, makeSpan};
+    }
+
+    return {Unsat, {}};
+}
+
+bool VarImportanceRunner::isInconsistent() const noexcept {
+    return inconsistent;
+}
