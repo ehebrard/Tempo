d27e90e314cf9ca439794bfd3e2a26b97abe5416
Tim Luchterhand <tim.luchterhand@laas.fr>
2024-08-13 14:27:32 +0200

    added precedence output to jsp parser

--- src/examples/helpers/scheduling_helpers.cpp
+++ src/examples/helpers/scheduling_helpers.cpp
@@ -14,18 +14,20 @@
 
 auto loadSchedulingProblem(const tempo::Options &options)
 -> std::tuple<SolverPtr, ProblemInstance, std::optional<int>> {
+    using Time = int;
     using namespace tempo;
     using namespace std::views;
     auto solver = std::make_unique<Solver<>>(options);
-    auto schedule{solver->newInterval(0, Constant::Infinity<int>, 0, 0, 0, Constant::Infinity<int>)};
-    std::vector<DisjunctiveResource<>> resources;
-    std::vector<Interval<>> tasks;
-    std::optional<int> optSol;
+    auto schedule{solver->newInterval(0, Constant::Infinity<Time>, 0, 0, 0, Constant::Infinity<Time>)};
+    std::vector<DisjunctiveResource<Time>> resources;
+    std::vector<Interval<Time>> tasks;
+    std::optional<Time> optSol;
+    std::vector<DistanceConstraint<Time>> precedences;
 
     if (options.input_format == "osp") {
         optSol = osp::parse(options.instance_file, *solver, schedule, tasks, resources);
     } else if (options.input_format == "jsp") {
-        jsp::parse(options.instance_file, *solver, schedule, tasks, resources);
+        jsp::parse(options.instance_file, *solver, schedule, tasks, resources, &precedences);
     } else {
         std::cerr << "problem type " << options.input_format << " is not (yet) supported" << std::endl;
         std::exit(1);
@@ -35,11 +37,11 @@ auto loadSchedulingProblem(const tempo::Options &options)
     for (const auto &consumingTasks: resources) {
         auto constraint = NoOverlap(schedule,
                                     consumingTasks | transform([&tasks](auto taskId) { return tasks.at(taskId); }),
-                                    std::vector<std::vector<int>>{});
+                                    std::vector<std::vector<Time>>{});
         solver->post(constraint);
     }
 
-    SchedulingProblemHelper problem(std::move(tasks), std::move(resources), {}, schedule);
+    SchedulingProblemHelper problem(std::move(tasks), std::move(resources), std::move(precedences), schedule);
     auto trivialUb = 0;
     for (const auto &t : problem.tasks()) {
         trivialUb += t.minDuration(*solver);
