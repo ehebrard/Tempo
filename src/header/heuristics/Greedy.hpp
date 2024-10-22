ehebrard/Tempo

commit 456f01b11d97839863c8980538a6ab1b21de57ff

fixed greedy after commt 6e84cb86bed3968cbe5b934e2ab9d83ec7bd630a

src/header/heuristics/Greedy.hpp
@@ -593,11 +593,12 @@ public:
         // std::cout << l << std::endl;
         // std::cout << solver.boolean.getEdge(l) << std::endl;
         auto pc{solver.boolean.getEdge(l)};
+        auto nc{solver.boolean.getEdge(~l)};
 
         // std::cout << precedences.size() << " - " << pc.to << std::endl;
         // std::cout << "("<< solver.numeric.size() << ")" << std::endl;
         precedences[pc.to].push_back(l);  
-        // precedences[nc.to].push_back((~l));
+         precedences[nc.to].push_back((~l));
 
         // std::cout << "prec maps " <<  pc.to << " - " << pc.from << std::endl;
 

commit 55a90c5a9daefc4202222a1a7ee28bec83aa2018

added guard to fix problem introduced with 456f01b11d97839863c8980538a6ab1b21de57ff

src/header/heuristics/Greedy.hpp
@@ -597,8 +597,13 @@ public:
 
         // std::cout << precedences.size() << " - " << pc.to << std::endl;
         // std::cout << "("<< solver.numeric.size() << ")" << std::endl;
-        precedences[pc.to].push_back(l);  
-         precedences[nc.to].push_back((~l));
+        if (pc != Constant::NoEdge<T>) {
+            precedences[pc.to].push_back(l);
+        }
+
+        if(nc != Constant::NoEdge<T>) {
+            precedences[nc.to].push_back((~l));
+        }
 
         // std::cout << "prec maps " <<  pc.to << " - " << pc.from << std::endl;
 
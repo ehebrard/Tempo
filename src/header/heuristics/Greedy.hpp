55a90c5a9daefc4202222a1a7ee28bec83aa2018
Tim Luchterhand <tim.luchterhand@laas.fr>
2024-10-22 16:58:42 +0200

    added guard to fix problem introduced with 456f01b11d97839863c8980538a6ab1b21de57ff

--- src/header/heuristics/Greedy.hpp
+++ src/header/heuristics/Greedy.hpp
@@ -597,8 +597,13 @@ public:
 
         // std::cout << precedences.size() << " - " << pc.to << std::endl;
         // std::cout << "("<< solver.numeric.size() << ")" << std::endl;
-        precedences[pc.to].push_back(l);  
-         precedences[nc.to].push_back((~l));
+        if (pc != Constant::NoEdge<T>) {
+            precedences[pc.to].push_back(l);
+        }
+
+        if(nc != Constant::NoEdge<T>) {
+            precedences[nc.to].push_back((~l));
+        }
 
         // std::cout << "prec maps " <<  pc.to << " - " << pc.from << std::endl;
 
