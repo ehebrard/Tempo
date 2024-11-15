32f6eb88d76224bac65378d31255d614457b204b
Tim Luchterhand <tim.luchterhand@laas.fr>
2024-11-15 12:15:54 +0100

    added distribution samplers + tests

--- src/cpp/util/random.cpp
+++ src/cpp/util/random.cpp
@@ -4,6 +4,11 @@
 * @brief
 */
 
+#include <algorithm>
+#include <cassert>
+#include <numeric>
+#include <Iterators.hpp>
+
 #include "util/random.hpp"
 
 namespace tempo {
@@ -31,4 +36,47 @@ namespace tempo {
     }
 
     auto RNG::operator()() const noexcept -> unsigned long { return random(); }
+
+    std::size_t DistributionSampler::random() const noexcept {
+        auto rVal = tempo::random() % cdf.back();
+        auto res = std::ranges::upper_bound(cdf, rVal);
+        assert(res != cdf.end());
+        return std::distance(cdf.begin(), res);
+    }
+
+    DistributionSampler::DistributionSampler(std::vector<unsigned long> cdf): cdf(std::move(cdf)) {
+        if (this->cdf.empty()) {
+            throw std::runtime_error("empty CDF");
+        }
+    }
+
+    auto DistributionSampler::getCDF() const noexcept -> const std::vector<unsigned long> & {
+        return cdf;
+    }
+
+    std::size_t ReplacementDistributionSampler::random() {
+        using namespace std::views;
+        if (exhausted()) {
+            throw std::runtime_error("sample is exhausted");
+        }
+
+        const auto idx = sampler.random();
+        unsigned long decrement = sampler.cdf[idx];
+        if (idx > 0) {
+            decrement -= sampler.cdf[idx - 1];
+        }
+
+        const auto ret = indices[idx];
+        auto source =
+                iterators::zip(sampler.cdf | transform([decrement](auto val) { return val - decrement; }), indices) |
+                drop(idx + 1);
+        auto destination = iterators::zip_i(sampler.cdf.begin() + idx, indices.begin() + idx);
+        std::ranges::copy(source, destination);
+        sampler.cdf.pop_back(); indices.pop_back();
+        return ret;
+    }
+
+    bool ReplacementDistributionSampler::exhausted() const noexcept {
+        return indices.empty();
+    }
 }
--- src/header/util/random.hpp
+++ src/header/util/random.hpp
@@ -10,6 +10,11 @@
 #include <limits>
 #include <ranges>
 #include <optional>
+#include <vector>
+#include <concepts>
+#include <stdexcept>
+
+#include "util/traits.hpp"
 
 namespace tempo {
 
@@ -21,12 +26,14 @@ namespace tempo {
      */
     void seed(unsigned long x_, unsigned long y_ = 362436069, unsigned long z_ = 521288629) noexcept;
 
+
     /**
      * create a random integer
      * @return random integer number
      * @note period of the generator is period 2^96-1
      */
     unsigned long random() noexcept;
+
 
     /**
      * helper method for triggering random events
@@ -39,6 +46,7 @@ namespace tempo {
         return random() % Res < static_cast<unsigned long>(probability * Res);
     }
 
+
     /**
      * @brief Random number generator that can be used in stl algorithms like std::shuffle
      */
@@ -50,18 +58,6 @@ namespace tempo {
         auto operator()() const noexcept -> unsigned long;
     };
 
-    /**
-     * Selects a random element from a range
-     * @tparam R range type
-     * @param range the range
-     * @param maxIdx optional maximum index (exclusive). UB if greater than size of range!
-     * @return random element from the range
-     */
-    template<std::ranges::random_access_range R> requires(std::ranges::sized_range<R>)
-    decltype(auto) random_select(R &&range, std::optional<std::size_t> maxIdx = {}) {
-        auto max = maxIdx.value_or(std::ranges::size(range));
-        return std::forward<R>(range)[random() % max];
-    }
 
     /**
      * selects a random iterator to an element in a range
@@ -73,8 +69,199 @@ namespace tempo {
     template<std::ranges::random_access_range R> requires(std::ranges::sized_range<R>)
     auto random_select_iterator(R &&range, std::optional<std::size_t> maxIdx = {}) {
         auto idx = random() % maxIdx.value_or(std::ranges::size(range));
-        return std::forward<R>(range).begin() + idx;
+        return std::ranges::begin(std::forward<R>(range)) + idx;
     }
+
+
+    /**
+     * Selects a random element from a range
+     * @tparam R range type
+     * @param range the range
+     * @param maxIdx optional maximum index (exclusive). UB if greater than size of range!
+     * @return random element from the range
+     */
+    template<std::ranges::random_access_range R> requires(std::ranges::sized_range<R>)
+    decltype(auto) random_select(R &&range, std::optional<std::size_t> maxIdx = {}) {
+        return *random_select_iterator(std::forward<R>(range), maxIdx);
+    }
+
+
+    namespace detail {
+        template<typename R>
+        concept integer_range = std::ranges::range<R> and std::integral<std::ranges::range_value_t<R>>;
+
+        template<typename R>
+        concept float_range = std::ranges::range<R> and std::floating_point<std::ranges::range_value_t<R>>;
+    }
+
+    class ReplacementDistributionSampler ;
+
+    /**
+     * @brief Samples from an arbitrary distribution function using inverse transform sampling.
+     * @details @copybrief
+     * This sampler is defined over an arbitrary PDF. It works the following way. Given the following PDF
+     *
+     * ^
+     * |            |
+     * |    |       |     |
+     * |    | |     | |   |
+     * |  | | | | | | | | | |
+     * |--0-1-2-3-4-5-6-7-8-9--> x
+     * the sampler selects values on the x-axis. In this example, the x-value 5 is the most likely
+     */
+    class DistributionSampler {
+        std::vector<unsigned long> cdf;
+        friend class ReplacementDistributionSampler;
+
+    protected:
+        [[nodiscard]] auto getCDF() const noexcept -> const std::vector<unsigned long>&;
+    public:
+        /**
+         * CTor
+         * @param cdf Cumulative density function. Does not need to sum up to one but needs to be a valid CDF
+         * otherwise, i.e. ordered in strictly ascending order. If not, this may lead to UB!
+         */
+        explicit DistributionSampler(std::vector<unsigned long> cdf);
+
+        /**
+         * Ctor
+         * @tparam R pdf range type
+         * @param pdf Probability density function. Does not need to sum to 1 but must not contain negative values
+         */
+        template<detail::integer_range R> requires(std::ranges::sized_range<R>)
+        explicit DistributionSampler(const R &pdf) {
+            unsigned long sum = 0;
+            cdf.reserve(std::ranges::size(pdf));
+            for (auto val : pdf) {
+                if (val < 0) {
+                    throw std::runtime_error("negative probability mass");
+                }
+
+                sum += val;
+                cdf.emplace_back(sum);
+            }
+
+            if (cdf.empty()) {
+                throw std::runtime_error("empty pdf");
+            }
+        }
+
+        /**
+         * Ctor
+         * @tparam R pdf range type
+         * @param pdf Probability density function. Does not need to sum to 1 but must not contain negative values
+         * @param precision fixed point precision (default 4 digits)
+         */
+        template<detail::float_range R>
+        requires(std::ranges::sized_range<R>)
+        explicit DistributionSampler(const R &pdf, unsigned long precision = 10000):
+                DistributionSampler(pdf | std::views::transform(
+                        [precision](auto flt) { return static_cast<unsigned long>(flt * precision); })) {}
+
+        /**
+         * Gets the x-index selected according to the probability distribution
+         * @return
+         */
+        [[nodiscard]] std::size_t random() const noexcept;
+
+        /**
+         * Selects a random iterator to an element in the given range according to the PDF
+         * @tparam R range type
+         * @param range the range
+         * @return iterator to element in the range
+         * @note The range must contain as many elements as the PDF
+         */
+        template<std::ranges::random_access_range R> requires(std::ranges::sized_range<R>)
+        auto randomSelectIterator(R &&range) {
+            if (std::ranges::size(std::forward<R>(range)) != cdf.size()) {
+                throw std::runtime_error("range needs to have as many elements as PDF");
+            }
+
+            return std::ranges::begin(std::forward<R>(range)) + this->random();
+        }
+
+        /**
+         * Selects a random element from the given range according to the PDF
+         * @tparam R range type
+         * @param range the range
+         * @return element in the range
+         * @note The range must contain as many elements as the PDF
+         */
+        template<std::ranges::random_access_range R> requires(std::ranges::sized_range<R>)
+        decltype(auto) randomSelect(R &&range) const {
+            return *this->randomSelectIterator(range);
+        }
+
+    };
+
+    /**
+     * @brief Samples WITH REPLACEMENT from an arbitrary distribution function using inverse transform sampling.
+     * @details @copybrief
+     * This class behaves similarly to DistributionSampler with the exceptions that samples are drawn WITH
+     * REPLACEMENT. This means that the distribution changes with each sample drawn
+     */
+    class ReplacementDistributionSampler {
+        std::vector<std::size_t> indices;
+        DistributionSampler sampler;
+    public:
+        /**
+         * Ctor. Same signatures as DistributionSampler
+         * @tparam Args argument types
+         * @param args arguments to DistributionSampler
+         */
+        template<typename ...Args>
+        explicit ReplacementDistributionSampler(Args &&...args): sampler(std::forward<Args>(args)...) {
+            std::ranges::iota_view values(std::size_t(0), sampler.cdf.size());
+            indices.assign(values.begin(), values.end());
+        }
+
+        /**
+         * Whether all values have been sampled
+         * @return true if all values have been drawn, false otherwise
+         */
+        [[nodiscard]] bool exhausted() const noexcept;
+
+        /**
+         * Draws an x-value according to the probability distribution. The same x value cannot be drawn twice.
+         * @return selected x-value
+         */
+        std::size_t random();
+
+        /**
+         * Selects a random iterator to an element in the given range according to the PDF. Each iterator can only be
+         * drawn once
+         * @tparam R range type
+         * @param range the range
+         * @return iterator to element in the range
+         * @note The range must contain at least as many elements as the PDF. You should not remove elements from your
+         * range. The sampler keeps track of the indices that have already been drawn
+         */
+        template<std::ranges::random_access_range R> requires(std::ranges::sized_range<R>)
+        auto randomSelectIterator(R &&range) {
+            if (exhausted()) {
+                throw std::runtime_error("sampler is exhausted");
+            }
+
+            if (std::ranges::size(std::forward<R>(range)) < indices.back()) {
+                throw std::runtime_error("range has too few elements");
+            }
+
+            return std::ranges::begin(std::forward<R>(range)) + this->random();
+        }
+
+        /**
+         * Selects a random element from the given range according to the PDF. Each element can only be chosen once.
+         * @tparam R range type
+         * @param range the range
+         * @return element in the range
+         * @note The range must contain at least as many elements as the PDF. You should not remove elements from your
+         * range. The sampler keeps track of the indices that have already been drawn
+         */
+        template<std::ranges::random_access_range R> requires(std::ranges::sized_range<R>)
+        decltype(auto) randomSelect(R &&range) {
+            return *this->randomSelectIterator(range);
+        }
+    };
 }
 
 #endif //TEMPO_RANDOM_HPP
