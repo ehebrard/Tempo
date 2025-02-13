45cd0a56a45dcca4443f46f522c0b306050392c8
Tim Luchterhand <tim.luchterhand@laas.fr>
2025-02-13 13:34:27 +0100

    printin support for tuples and pairs

--- src/header/util/printing.hpp
+++ src/header/util/printing.hpp
@@ -10,8 +10,41 @@
 
 #include <ranges>
 #include <ostream>
+#include <tuple>
+#include <utility>
 
-template<std::ranges::range R>
+#include "util/traits.hpp"
+
+namespace detail {
+    template<std::size_t Idx, typename Tuple>
+    void printElem(std::ostream& os, const Tuple &tuple) {
+        if constexpr (Idx != 0) {
+            os << ", ";
+        }
+
+        os << std::get<Idx>(tuple);
+    }
+}
+
+namespace std {
+    template<tempo::concepts::printable ...Ts>
+    std::ostream &operator<<(std::ostream &os, const std::tuple<Ts...> &tuple) {
+        os << "(";
+        [&]<std::size_t ... Idx>(std::index_sequence<Idx...>) {
+            (detail::printElem<Idx>(os, tuple), ...);
+        }(std::make_index_sequence<sizeof...(Ts)>());
+        os << ")";
+        return os;
+    }
+
+    template<tempo::concepts::printable T, tempo::concepts::printable U>
+    std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &pair) {
+        os << "(" << pair.first << ", " << pair.second << ")";
+        return os;
+    }
+}
+
+template<std::ranges::range R> requires(tempo::concepts::printable<std::ranges::range_value_t<R>>)
 auto printRange(const R &range, std::ostream &os) -> std::ostream& {
     os << "[";
     bool first = true;
