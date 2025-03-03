fbefa6df55623c9fc0830b2f1e60fc189368569f
Tim Luchterhand <tim.luchterhand@laas.fr>
2025-03-03 11:22:24 +0100

    fixed compilation

--- src/header/heuristics/LNS/relaxation_evaluators.hpp
+++ src/header/heuristics/LNS/relaxation_evaluators.hpp
@@ -451,7 +451,8 @@ namespace tempo::lns {
                 }
             }
 
-            regions.emplace_back(std::move(constraints), proxy.getSolver().numeric.lower(objectiveVar) - 1, true);
+            regions.emplace_back(std::move(constraints), proxy.getSolver().numeric.solutionLower(objectiveVar) - 1,
+                                 true);
         }
 
         void notifySuccess(unsigned numFails) {
