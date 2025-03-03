d6195890fe163180c964cee19cf70fb6f63c8e84
Tim Luchterhand <tim.luchterhand@laas.fr>
2025-03-03 18:44:28 +0100

    print deduced instance file in compute accuracy

--- src/examples/compute_accuracy.cpp
+++ src/examples/compute_accuracy.cpp
@@ -1024,6 +1024,7 @@ int main(int argc, char *argv[]) {
 
     const auto instanceName = match[0].str();
     opt.instance_file = fs::path(instanceDir) / instanceName;
+    std::cout << "-- deduced instance file " << opt.instance_file << std::endl;
   }
 
 
