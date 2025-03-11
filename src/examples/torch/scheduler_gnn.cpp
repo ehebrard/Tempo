fe34b8811239be262419f37ee4c264680b8c0c86
Tim Luchterhand <tim.luchterhand@laas.fr>
2025-03-11 17:55:31 +0100

    updated gnn scheduler, added very simple mechanism to reduce GNN calls, not very good

--- src/examples/torch/scheduler_gnn.cpp
+++ src/examples/torch/scheduler_gnn.cpp
@@ -19,17 +19,21 @@ int main(int argc, char **argv) {
     using namespace heuristics;
     std::string gnnLocation;
     std::string featureExtractorConf;
+    float maxFailRatio;
     auto opt = cli::parseOptions(argc, argv,
                                  cli::ArgSpec("gnn-loc", "Location of the GNN model", true, gnnLocation),
                                  cli::ArgSpec("feat-config", "Location of the feature extractor config", true,
-                                              featureExtractorConf));
+                                              featureExtractorConf),
+                                 cli::ArgSpec("gnn-max-fail-ratio", "maximum fail ratio before inference is rerun",
+                                              false, maxFailRatio, 0.2));
 
     auto [solver, problem, _, _1, _2, _3] = loadSchedulingProblem(opt);
     auto schedule = problem.schedule();
     using PType = decltype(problem);
     using GNNH = util::ProfiledHeuristic<nn::GNNFullGuidance<PType::Time, PType::Resource>>;
     util::Profiler profiler;
-    GNNH valBranching(profiler, opt.polarity_epsilon, gnnLocation, featureExtractorConf, std::move(problem));
+    GNNH valBranching(profiler, opt.polarity_epsilon, gnnLocation, featureExtractorConf, *solver, maxFailRatio,
+                      std::move(problem));
     auto varBranching = make_variable_heuristic(*solver);
     solver->setBranchingHeuristic(make_compound_heuristic(std::move(varBranching), std::move(valBranching)));
     solver->minimize(schedule.duration);
--- src/header/nn/GNNValueHeuristics.hpp
+++ src/header/nn/GNNValueHeuristics.hpp
@@ -8,20 +8,17 @@
 #define TEMPO_GNNVALUEHEURISTICS_HPP
 
 #include <filesystem>
+#include <iostream>
 
 #include "util/traits.hpp"
 #include "util/SubscribableEvent.hpp"
 #include "Global.hpp"
+#include "Solver.hpp"
 #include "nn/GNNEdgePolarityPredictor.hpp"
 #include "heuristics/BaseBooleanHeuristic.hpp"
 #include "util/SchedulingProblemHelper.hpp"
 
 namespace fs = std::filesystem;
-
-namespace tempo {
-    template<typename T>
-    class Solver;
-}
 
 namespace tempo::nn {
 
@@ -34,9 +31,21 @@ namespace tempo::nn {
      * @note This Value branching heuristic is only ment to be used for scheduling problems
      */
     template<concepts::scalar T, SchedulingResource R>
-    class GNNFullGuidance: public heuristics::BaseBooleanHeuristic<GNNFullGuidance<T, R>> {
-        nn::GNNEdgePolarityPredictor<T, R> polarityPredictor;
+    class GNNFullGuidance: public heuristics::BaseBooleanHeuristic<GNNFullGuidance<T, R>, T> {
+        GNNEdgePolarityPredictor<T, R> polarityPredictor;
+        SubscriberHandle failHandler;
+        SubscriberHandle successHandler;
+        unsigned numFails = 0;
+        unsigned numVars;
+        double maxFailRatio;
+        bool decisionFlag = false;
     public:
+
+        GNNFullGuidance(const GNNFullGuidance &) = delete;
+        GNNFullGuidance(GNNFullGuidance &&) = delete;
+        GNNFullGuidance &operator=(const GNNFullGuidance &) = delete;
+        GNNFullGuidance &operator=(GNNFullGuidance &&) = delete;
+        ~GNNFullGuidance() override = default;
 
         /**
          * Ctor
@@ -44,12 +53,29 @@ namespace tempo::nn {
          * @param modelLocation location of the trained GNN model
          * @param featureExtractorConfigLocation location of the feature extractor configuration
          * (tempo::nn:GraphBuilderConfig)
+         * @param solver target solver
+         * @param maxFailRatio maximum fail ratio (num decision fails divided by num variables) at which the GNN is
+         * reevaluated
          * @param problem initial description of the problem
          */
         GNNFullGuidance(double epsilon, const fs::path &modelLocation, const fs::path &featureExtractorConfigLocation,
-                        SchedulingProblemHelper<T, R> problem)
-                : heuristics::BaseBooleanHeuristic<GNNFullGuidance<T, R>>(epsilon),
-                  polarityPredictor(modelLocation, featureExtractorConfigLocation, std::move(problem)) {}
+                        const Solver<T> &solver, float maxFailRatio, SchedulingProblemHelper<T, R> problem)
+            : heuristics::BaseBooleanHeuristic<GNNFullGuidance, T>(epsilon),
+              polarityPredictor(modelLocation, featureExtractorConfigLocation, std::move(problem)),
+              failHandler(solver.ConflictEncountered.subscribe_handled([this](auto &&) {
+                  ++numFails;
+                  decisionFlag = false;
+              })),
+              successHandler(solver.PropagationCompleted.subscribe_handled([this](auto &&) {
+                  if (decisionFlag and numFails > 0) { --numFails; }
+              })),
+              numVars(solver.boolean.size()), maxFailRatio(maxFailRatio) {
+            polarityPredictor.preEvaluation(solver);
+        }
+
+        double failRatio() const noexcept {
+            return numFails / static_cast<double>(numVars);
+        }
 
         /**
          * BaseBooleanHeuristic interface
@@ -58,7 +84,15 @@ namespace tempo::nn {
          * @returns branching Literal
          */
         auto choose(var_t x, const Solver<T> &solver) -> Literal<T> {
-            polarityPredictor.preEvaluation(solver);
+            decisionFlag = true;
+            if (failRatio() > maxFailRatio) {
+                if (solver.getOptions().verbosity >= Options::YACKING) {
+                    std::cout << "-- rerunning GNN inference" << std::endl;
+                }
+                polarityPredictor.preEvaluation(solver);
+                numFails = 0;
+            }
+
             return polarityPredictor.choose(x, solver);
         }
     };
